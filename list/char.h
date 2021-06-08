7adbfa58dbb2a46ed5a6e89c6a1844a5a985104f
hacker3983 <mrgamer19554@gmail.com>
2021-06-08 16:42:52 -0500

    Update list/char.h

--- list/char.h
+++ list/char.h
@@ -7,7 +7,7 @@
 typedef struct char_s {
 	char* buff;
 	size_t size;
-	int idx;
+	int length;
 	int init;
 } char_t;
 
@@ -15,15 +15,15 @@ typedef struct char_s {
 void char_init(char_t* mychar) {
 	mychar->buff = calloc(1, sizeof(char*));
 	mychar->size = 1;
-	mychar->idx = 0;
+	mychar->length = 0;
 	mychar->init = 1;
 }
 // push a character to the structure
 bool char_push(char_t* mychar, char c) {
 	if(mychar->init == 1) {
-		mychar->buff[mychar->idx] = c;
+		mychar->buff[mychar->length] = c;
 		mychar->size++;
-		mychar->idx++;
+		mychar->length++;
 		char* ptr = mychar->buff;
 		mychar->buff = calloc(mychar->size, sizeof(char*));
 		strcpy(mychar->buff, ptr);
@@ -40,18 +40,29 @@ bool char_pushptr(char_t* mychar, char* buff) {
 		mychar->buff = calloc(mychar->size, sizeof(char*));
 		strcpy(mychar->buff, ptr);
 		strcat(mychar->buff, buff);
-		mychar->idx += strlen(buff);
+		mychar->length += strlen(buff);
 		free(ptr);
 		return true;
 	}
 	return false;
+}
+void char_reverse(char_t* mychar) {
+	char_t list;
+	char_init(&list);
+	for(int i=mychar->length-1;i>=0;i--) {
+		char_push(&list, mychar->buff[i]);
+	}
+	free(mychar->buff);
+	mychar->size = list.size;
+	mychar->length = list.length;
+	mychar->buff = list.buff;
 }
 // frees memory and resets everything
 void char_free(char_t* mychar) {
 	if(mychar->init == 1) {
 		free(mychar->buff);
 		mychar->size = 0;
-		mychar->idx = 0;
+		mychar->length = 0;
 		mychar->init = 0;
 	}
 }
